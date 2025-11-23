406a5c93c650a72cd56b34c291fb965191a62afd
UTKARSH SINHA <utkarshsinha@UTKARSHs-MacBook-Air.local>
2025-11-23 21:02:01 +0530

    DataTypes

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId=144553;//value cannot be changed
+let accountEmail="off.utkarsh.sinha@gmail.com";
+var accountPassword="kishusinha123";
+accountCity="Bhubaneswar";
+
+// accountId=2;//throws error because value cannot be changed
+accountEmail="sinhau65@gmail.com";
+accountPassword="2005";
+accountCity="Kolkata";
+console.log(accountId);
+/*
+Multiline Comments 
+Note : Prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ datatypes.js
@@ -0,0 +1,27 @@
+"use strict";//treats all JS code as newer version
+
+//alert(3+3);//we are using nodejs, not browser
+// readability should be high
+console.log(3
+    +9);console.log("Utkarsh ");
+// improve readibility of code
+console.log(9+9);
+console.log("Utkarsh Sinha");
+//https://tc39.es
+
+// let name="Utkarsh"; String Data Type
+// let age=20; Number Data Type
+// let isLoggedIn=false; Boolean Data Type
+
+// number=>2 to power 53
+// bigint=>for large numbers
+// string=>""
+// boolean=>true/false
+// null=>standalone value
+// undefined=>value not defined
+// symbol=>unique
+// object=>key:value pair
+let state;//undefined
+let states=null;
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
